2f4530b93491e78e46b8c33f64ec103f1aba0775
emilyruby <zcabeme@ucl.ac.uk>
2017-08-20 14:13:57 +0100

    new dependencies

--- resauce/src/main.js
+++ resauce/src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueFire from 'vuefire'
+import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
+
+Vue.use(VueFire)
+Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
