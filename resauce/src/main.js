abe04ed632096e75aba8e8eab9066e9fdd3de8b4
emilyruby <zcabeme@ucl.ac.uk>
2017-09-21 22:07:18 +0100

    new modal library

--- resauce/src/main.js
+++ resauce/src/main.js
@@ -1,20 +1,39 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueFire from 'vuefire'
 import Vuetify from 'vuetify'
+import VueResource from 'vue-resource'
+import VueAuthenticate from 'vue-authenticate'
+import VModal from 'vue-js-modal'
 
 Vue.config.productionTip = false
 
+Vue.use(VModal, { dialog: true })
 Vue.use(VueFire)
 Vue.use(Vuetify)
+Vue.use(VueResource)
+Vue.use(VueAuthenticate, {
+  baseUrl: 'http://localhost:8080', // Your API domain
+
+  providers: {
+    twitter: {
+      clientId: '',
+      redirectUri: 'http://localhost:8080/auth/callback' // Your client app URL
+    }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  methods: {
+    authenticate: function (provider) {
+      this.$auth.authenticate(provider).then(function () {
+      })
+    }
+  }
 })
