708e3cfd88af34ce564627b66d0ae9e20d122fc1
emilyruby <zcabeme@ucl.ac.uk>
2017-09-23 22:15:14 +0100

    router callback

--- resauce/src/router/index.js
+++ resauce/src/router/index.js
@@ -1,15 +1,20 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import MainPage from '@/components/MainPage.vue'
 
-Vue.use(Router)
-
-export default new Router({
+const router = new VueRouter({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'MainPage',
       component: MainPage
+    },
+    {
+      path: '/auth/callback',
+      component: {
+        template: '<div class="auth-component"></div>'
+      }
     }
   ]
 })
+
+export default router
